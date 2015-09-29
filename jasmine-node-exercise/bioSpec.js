c378323a5f68ed5f23161ad1029e575f0d5499b6
Jean Carlo Rodriguez <alejandrojeancarlo@gmail.com>
2015-09-29 15:47:42 -0400

    finished

--- jasmine-node-exercise/bioSpec.js
+++ jasmine-node-exercise/bioSpec.js
@@ -3,7 +3,7 @@ var Bio = require('./bio.js');
 //console.log(bio); 
 
 describe('getBio',function(){
-	xit('verify that at send the name Jean Carlo and the age 12 show a message of the bio',function(){
+	it('verify that at send the name Jean Carlo and the age 12 show a message of the bio',function(){
 		var actualResult = Bio('Jean Carlo',12);	
 		var expectedResult = 'Hi, my name is Jean Carlo, I am 12 years old and Iam a nice guy.';
 		expect(expectedResult).toEqual(actualResult);
@@ -11,12 +11,14 @@ describe('getBio',function(){
 	
 	it('Verify that at no introduce the name of the person an error is throw',function(){
 		
-		var actualResult = Bio();	
-		var expectedResult = 'Need name and age to provide a biography';
 		expect(function(){Bio()}).toThrow('Need name and age to provide a biography');
-		//expect(expectedResult).toEqual(actualResult);
+		
 	});
-	
+	it('Verify that at introduce the value of age lower than 0 a message is throw',function(){
+		
+		expect(function(){Bio('Jean carlo',-2)}).toThrow('Invalid age.');
+		
+	});
 	
 	
 	
